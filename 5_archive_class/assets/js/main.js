6dc105c0adc4c623826ccd038869a1f305865494
Alice Tsai <htsa12@student.monash.edu>
2018-04-19 16:16:55 +1000

    update

---
+++ 5_archive_class/assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+$('.b-red').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.red').addClass('show');
+  $('.item.red').addClass('col3of4');
+  $('.item.red').removeClass('hide');
+});
+
+
+$('.sample').click(function(){
+
+});
+
+$('.sample').addClass('classname');
+$('.sample').removeClass('classname');
+
+
+
+
+
+  });
---
+++ 6_archive/assets/js/main.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+$('.b-flower').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.flower').addClass('show');
+  $('.item.flower').addClass('col2of5');
+  $('.item.flower').removeClass('hide');
+});
+
+$('.b-rainbow').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.rainbow').addClass('show');
+  $('.item.rainbow').addClass('col2of5');
+  $('.item.rainbow').removeClass('hide');
+});
+
+$('.b-creative').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.creative').addClass('show');
+  $('.item.creative').addClass('col2of5');
+  $('.item.creative').removeClass('hide');
+});
+
+$('.b-icetower').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.icetower').addClass('show');
+  $('.item.icetower').addClass('col2of5');
+  $('.item.icetower').removeClass('hide');
+});
+
+$('.b-beauty').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.beauty').addClass('show');
+  $('.item.beauty').addClass('col2of5');
+  $('.item.beauty').removeClass('hide');
+});
+
+$('.b-advertisment').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.advertisment').addClass('show');
+  $('.item.advertisment').addClass('col2of5');
+  $('.item.advertisment').removeClass('hide');
+});
+
+$('.b-history').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.history').addClass('show');
+  $('.item.history').addClass('col2of5');
+  $('.item.history').removeClass('hide');
+});
+
+$('.b-painting').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.painting').addClass('show');
+  $('.item.painting').addClass('col2of5');
+  $('.item.painting').removeClass('hide');
+});
+
+$('.b-illustration').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.illustration').addClass('show');
+  $('.item.illustration').addClass('col2of5');
+  $('.item.illustration').removeClass('hide');
+});
+
+$('.sample').click(function(){
+
+});
+
+$('.sample').addClass('classname');
+$('.sample').removeClass('classname');
+
+
+
+
+
+  });
