3eb2199beebf927ab308ba5310ff2812be4455c2
Alice Tsai <htsa12@student.monash.edu>
2018-05-22 17:37:55 +1000

    upload

---
+++ 7_reader/assets/js/main.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+$('.nav').click(function() {
+  $('.document').toggleClass('showme');
+});
+
+
+$('.r-richard').mouseenter(function() {
+   $('.p-richard').addClass('p-show');
+ });
+
+$('.r-richard').mouseleave(function() {
+   $('.p-richard').removeClass('p-show');
+ });
+
+ $('.r-DNA').mouseenter(function() {
+    $('.p-DNA').addClass('p-show');
+  });
+
+ $('.r-DNA').mouseleave(function() {
+    $('.p-DNA').removeClass('p-show');
+  });
+
+  $('.r-gene').mouseenter(function() {
+     $('.p-gene').addClass('p-show');
+   });
+
+  $('.r-gene').mouseleave(function() {
+     $('.p-gene').removeClass('p-show');
+   });
+
+   $('.r-selfish').mouseenter(function() {
+      $('.p-selfish').addClass('p-show');
+    });
+
+   $('.r-selfish').mouseleave(function() {
+      $('.p-selfish').removeClass('p-show');
+    });
+
+    $('.r-species').mouseenter(function() {
+       $('.p-species').addClass('p-show');
+     });
+
+    $('.r-species').mouseleave(function() {
+       $('.p-species').removeClass('p-show');
+     });
+     $('.r-meme').mouseenter(function() {
+        $('.p-meme').addClass('p-show');
+      });
+
+     $('.r-meme').mouseleave(function() {
+        $('.p-meme').removeClass('p-show');
+      });
+
+      $('.r-shortleg').mouseenter(function() {
+         $('.p-shortleg').addClass('p-show');
+       });
+
+      $('.r-shortleg').mouseleave(function() {
+         $('.p-shortleg').removeClass('p-show');
+       });
+       $('.r-zebra').mouseenter(function() {
+          $('.p-zebra').addClass('p-show');
+        });
+
+       $('.r-zebra').mouseleave(function() {
+          $('.p-zebra').removeClass('p-show');
+        });
+        $('.r-lion').mouseenter(function() {
+           $('.p-lion').addClass('p-show');
+         });
+
+        $('.r-lion').mouseleave(function() {
+           $('.p-lion').removeClass('p-show');
+         });
+         $('.r-slowly').mouseenter(function() {
+            $('.p-slowly').addClass('p-show');
+          });
+
+         $('.r-slowly').mouseleave(function() {
+            $('.p-slowly').removeClass('p-show');
+          });
+
+
+// please dont delete this line
+ });
