512a8d59c8d8eb353b3a927c01eae7c65f12bb16
Alice Tsai <htsa12@student.monash.edu>
2018-05-24 16:59:16 +1000

    upload

--- 7_reader/assets/js/main.js
+++ 7_reader/assets/js/main.js
@@ -121,5 +121,29 @@ $('.r-richard').mouseleave(function() {
               $('.r-language').mouseleave(function() {
                  $('.p-language').removeClass('p-show');
                });
+
+               $('.r-fashion').mouseenter(function() {
+                  $('.p-fashion').addClass('p-show');
+                });
+
+               $('.r-fashion').mouseleave(function() {
+                  $('.p-fashion').removeClass('p-show');
+                });
+
+                $('.r-religion').mouseenter(function() {
+                   $('.p-religion').addClass('p-show');
+                 });
+
+                $('.r-religion').mouseleave(function() {
+                   $('.p-religion').removeClass('p-show');
+                 });
+
+                 $('.r-sport').mouseenter(function() {
+                    $('.p-sport').addClass('p-show');
+                  });
+
+                 $('.r-sport').mouseleave(function() {
+                    $('.p-sport').removeClass('p-show');
+                  });
 // please dont delete this line
  });
